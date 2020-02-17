6b0cbf552e93cdd09d0272d07934f194ef4a1c8e
gpietrafesavieitez <gpietrafesavieitez@danielcastelao.org>
2020-02-17 13:35:51 +0100

    comentado fragment galeria

--- app/src/main/java/com/example/navigationdraweractivity/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/navigationdraweractivity/ui/gallery/GalleryFragment.kt
@@ -10,19 +10,23 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.navigationdraweractivity.R
 
+//  Fragment de la galería
 class GalleryFragment : Fragment() {
-
+    //  Se declara una variable que contendrá el modelo correspondiente
     private lateinit var galleryViewModel: GalleryViewModel
 
+    //  Sobreescribe un método propio de la clase Fragment
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        //  Se instancia la variable para el modelo para recogerlo y almacenarlo en la misma
         galleryViewModel =
             ViewModelProviders.of(this).get(GalleryViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_gallery, container, false)
         val textView: TextView = root.findViewById(R.id.text_gallery)
+        //  Se crea un observador para tener en cuenta el texto del modelo
         galleryViewModel.text.observe(this, Observer {
             textView.text = it
         })
