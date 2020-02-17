45d60e2731746657cc09ec345d70802f67f69daf
gpietrafesavieitez <gpietrafesavieitez@danielcastelao.org>
2020-02-17 13:44:43 +0100

    comentado fragment tools

--- app/src/main/java/com/example/navigationdraweractivity/ui/tools/ToolsFragment.kt
+++ app/src/main/java/com/example/navigationdraweractivity/ui/tools/ToolsFragment.kt
@@ -10,19 +10,24 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.navigationdraweractivity.R
 
+//  Fragment para el apartado de opciones
 class ToolsFragment : Fragment() {
 
+    //  Se declara una variable que contendrá el modelo correspondiente
     private lateinit var toolsViewModel: ToolsViewModel
 
+    //  Sobreescribe un método propio de la clase Fragment
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        //  Se instancia la variable para el modelo para recogerlo y almacenarlo en la misma
         toolsViewModel =
             ViewModelProviders.of(this).get(ToolsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_tools, container, false)
         val textView: TextView = root.findViewById(R.id.text_tools)
+        //  Se crea un observador para tener en cuenta el texto del modelo
         toolsViewModel.text.observe(this, Observer {
             textView.text = it
         })
