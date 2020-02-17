355044502c7b06fb00299876b06310f34f21c7d7
gpietrafesavieitez <gpietrafesavieitez@danielcastelao.org>
2020-02-17 13:17:52 +0100

    comentada clase principal

--- app/src/main/java/com/example/navigationdraweractivity/MainActivity.kt
+++ app/src/main/java/com/example/navigationdraweractivity/MainActivity.kt
@@ -15,22 +15,29 @@ import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
-
+    //  Define el menú desplegable horizontal que será el componente principal
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //  Se activa el layout correspondiente para esta actividad (activiy_main.xml)
         setContentView(R.layout.activity_main)
+        //  Declara toolbar para usarla como barra superior personalizada
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+        /*  Se define el botón flotante junto con su escuchador de eventos el cual
+            tiene como acción por defecto crear y mostrar un objeto Snackbar que
+            se mostrará en la parte inferior de la pantalla por unos segundos   */
         val fab: FloatingActionButton = findViewById(R.id.fab)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+        //  Se declaran los demás componentes y se conectan a sus respectivos layouts
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
+        //  Se instancia el componen
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
@@ -42,11 +49,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        /*  Sobreescribe un método propio de la clase Activity para añadir los
+            items al menu menu desplegable horizontal si es que existen     */
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
 
     override fun onSupportNavigateUp(): Boolean {
+        /*  Sobreescribe un método propio de la clase AppCompatActivity (heredada directamente de
+            Activity) que es invocado cuando se navega desde el menú desplegable horizontal    */
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
