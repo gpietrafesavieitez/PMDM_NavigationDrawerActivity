573dfa92b6e5be491d114656fefe5d3b64b26d6c
gpietrafesavieitez <gpietrafesavieitez@danielcastelao.org>
2020-02-17 13:40:46 +0100

    comentado fragment home

--- app/src/main/java/com/example/navigationdraweractivity/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/navigationdraweractivity/ui/home/HomeFragment.kt
@@ -10,19 +10,24 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.navigationdraweractivity.R
 
+//  Fragment para el menú principal
 class HomeFragment : Fragment() {
 
+    //  Se declara una variable que contendrá el modelo correspondiente
     private lateinit var homeViewModel: HomeViewModel
 
+    //  Sobreescribe un método propio de la clase Fragment
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        //  Se instancia la variable para el modelo para recogerlo y almacenarlo en la misma
         homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
+        //  Se crea un observador para tener en cuenta el texto del modelo
         homeViewModel.text.observe(this, Observer {
             textView.text = it
         })
