f2c723812031d3fcd67a5bd2694c19b0571202e4
gpietrafesavieitez <gpietrafesavieitez@danielcastelao.org>
2020-02-17 13:43:12 +0100

    comentado fragment share

--- app/src/main/java/com/example/navigationdraweractivity/ui/share/ShareFragment.kt
+++ app/src/main/java/com/example/navigationdraweractivity/ui/share/ShareFragment.kt
@@ -10,19 +10,24 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.navigationdraweractivity.R
 
+//  Fragment para la opción de compartir
 class ShareFragment : Fragment() {
 
+    //  Se declara una variable que contendrá el modelo correspondiente
     private lateinit var shareViewModel: ShareViewModel
 
+    //  Sobreescribe un método propio de la clase Fragment
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        //  Se instancia la variable para el modelo para recogerlo y almacenarlo en la misma
         shareViewModel =
             ViewModelProviders.of(this).get(ShareViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_share, container, false)
         val textView: TextView = root.findViewById(R.id.text_share)
+        //  Se crea un observador para tener en cuenta el texto del modelo
         shareViewModel.text.observe(this, Observer {
             textView.text = it
         })
