d6bdd5fd73b85544f93ed9a8762a15c3b5f97002
gpietrafesavieitez <gpietrafesavieitez@danielcastelao.org>
2020-02-17 13:47:50 +0100

    comentado fragment slideshow

--- app/src/main/java/com/example/navigationdraweractivity/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/com/example/navigationdraweractivity/ui/slideshow/SlideshowFragment.kt
@@ -10,19 +10,24 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.navigationdraweractivity.R
 
+//  Fragment de las diapositivas
 class SlideshowFragment : Fragment() {
 
+    //  Se declara una variable que contendrá el modelo correspondiente
     private lateinit var slideshowViewModel: SlideshowViewModel
 
+    //  Sobreescribe un método propio de la clase Fragment
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        //  Se instancia la variable para el modelo para recogerlo y almacenarlo en la misma
         slideshowViewModel =
             ViewModelProviders.of(this).get(SlideshowViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
         val textView: TextView = root.findViewById(R.id.text_slideshow)
+        //  Se crea un observador para tener en cuenta el texto del modelo
         slideshowViewModel.text.observe(this, Observer {
             textView.text = it
         })
